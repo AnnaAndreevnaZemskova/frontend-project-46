276cb799b135884ed71a403714cb8005ba9f7e36
Анна Земскова <anuta.nyta1012@yandex.ru>
2024-09-25 15:15:55 +0500

    step2

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
-import { Command } from 'commander';
-
-const program = new Command();
+// import { Command } from 'commander';
+// const program = new Command();
+const { program } = require('commander');
 
 program
   .version('0.0.1', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format <type>', 'output format');
 program.parse();
